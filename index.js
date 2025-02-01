97b843fcb0a49dcecb731ccb06725136d6f14da2
Muhammad Hammad Uddin <hammaduddin083@gmail.com>
2025-02-01 14:44:39 +0500

    commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ connect();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'hackathonfrontend-gilt.vercel.app/', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true,  
 }));
