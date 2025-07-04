cd984185bd35ec5bad0d4af88fb71ac3d6f55288
Muhammad Hammad Uddin <156169531+Muhammad-HammadUddin@users.noreply.github.com>
2025-07-04 19:31:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ connect();
 const app = express();
 
 app.use(cors({
-  origin: '*',
+  origin: 'https://frontend-alpha-ten-96.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
