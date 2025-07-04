a1938d327db2dc9623e4c2f07238dcdbd7d4190c
Muhammad Hammad Uddin <hammaduddin083@gmail.com>
2025-07-04 19:20:45 +0500

    update json

--- index.js
+++ index.js
@@ -54,3 +54,6 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+app.get("/", (req, res) => {
+  res.send("Server is running ✅");
+});
